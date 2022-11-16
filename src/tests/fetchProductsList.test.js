5ace9420b3c670c1ffc94a36bff0430218786cab
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-11-16 16:08:38 -0300

    added tests fetchProductsList

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,25 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled()
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(endpoint);
   });
 
-  // it('...', () => {
-  // });
-});
+  it('A função fetchProductList esta retornando o esperado', async () => {
+    expect(await fetchProductsList('computador')).toEqual(computadorSearch);
+  });
+
+  it('ao chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: "Termo de busca não informado"', async () => {
+    expect(await fetchProductsList()).toThrowError('Tremo de busca não informado');
+  });
+})
