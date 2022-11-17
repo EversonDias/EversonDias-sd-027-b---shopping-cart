762570c2de950dca1673bdc790acc976d642a09c
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-11-17 13:17:09 -0300

    added loading

--- src/main.js
+++ src/main.js
@@ -2,21 +2,26 @@ import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
+import { Loader } from './tools/loader';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+Loader.open()
+
 const fullProductList = await fetchProductsList('computador');
+
+Loader.close()
 
 fullProductList.forEach(data => {
   document.querySelector('.products')
-  .appendChild(
-    createProductElement(
-      {
-    id: data.id,
-    title: data.title,
-    thumbnail: data.thumbnail,
-    price: data.price,
-  }
-  ));
-  
+    .appendChild(
+      createProductElement(
+        {
+          id: data.id,
+          title: data.title,
+          thumbnail: data.thumbnail,
+          price: data.price,
+        }
+      ));
 })
+
